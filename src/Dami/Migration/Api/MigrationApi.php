df7bef6a7c8eaa2eac35d28c049504ac19b98891
Arek Jaskólski <arek.jaskolski@gmail.com>
2013-09-15 23:59:53 +0200

    Added execute method to api

--- src/Dami/Migration/Api/MigrationApi.php
+++ src/Dami/Migration/Api/MigrationApi.php
@@ -10,15 +10,28 @@ use Rentgen\Database\Constraint\PrimaryKey;
 
 abstract class MigrationApi
 {	
-	public $actions = null;
+	private $actions = null;
 	private $manipulation;
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param Manipulation $manipulation Manipulation instance.
+	 */
 	public function __construct(Manipulation $manipulation)
 	{						
 		$this->actions = array();
 		$this->manipulation = $manipulation;
 	}
 
+	/**
+	 * Create new table.
+	 * 
+	 * @param  string $name    Table name.
+	 * @param  array  $options Optional options.
+	 * 
+	 * @return Table           Table instance.
+	 */
 	public function createTable($name, array $options = array())
 	{		
 		$table = new Table($name);					
@@ -32,15 +45,27 @@ abstract class MigrationApi
 		return $table;
 	}
 
-	public function dropTable($name)
+	/**
+	 * Drop table.
+	 * 
+	 * @param  string  $name    Table name.
+	 * @param  boolean $cascade If delete cascade.
+	 * 
+	 * @return void
+	 */
+	public function dropTable($name, $cascade = true)
 	{		
 		$table = new Table($name);			
 		$this->actions[] =  function () use($table) {
-    		return $this->manipulation->dropTable($table, true);    			
-    	};
-		return $table;
+    		return $this->manipulation->dropTable($table, cascade);    			
+    	};		
 	}	
 
+	/**
+	 * Add foreign key.
+	 *
+	 * @return ForeignKey ForeignKey instance.
+	 */
 	public function addForeignKey()	
 	{
 		$foreignKey = new ForeignKey(new Table('foo'), new Table('fof'));
@@ -51,6 +76,23 @@ abstract class MigrationApi
 		return $foreignKey;
 	}
 
+	/**
+	 * Execute SQL.
+	 * 
+	 * @param  string $sql SQL to execute.
+	 * 
+	 * @return integer
+	 */
+	public function execute($sql)
+	{
+		return $this->manipulation->execute($sql);
+	}
+
+	/**
+	 * Get actions to execute.
+	 * 
+	 * @return array Actions to execute.
+	 */
 	public function getActions()
 	{
 		return $this->actions;
