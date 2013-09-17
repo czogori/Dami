24a27d16e655b4f0cbdf5be4a043fbf531109255
Arek Jaskólski <arek.jaskolski@gmail.com>
2013-09-18 00:38:53 +0200

    Added some tests

---
+++ tests/Migration/Api/MigrationApiTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Dami\Tests\Migration;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Dami\Migration\Api\MigrationApi;
+
+/**
+ * @author Arek Jaskólski <arek.jaskolski@gmail.com>
+ */
+class MigrationApiTest extends \PHPUnit_Framework_TestCase
+{   	
+	protected function setUp()
+    {
+        $manipulation = $this->getMock('Rentgen\Schema\Manipulation', null, array(new ContainerBuilder()));
+        $manipulation
+            ->expects($this->any())
+            ->method('createTable')
+            ->will($this->returnValue('Dami\Migration\Api\Table'));
+        
+        $this->api = new FooApi($manipulation);
+    }
+	public function testCreateTable()
+    {                    	    	
+    	$this->assertInstanceOf('Dami\Migration\Api\Table', $this->api->createTable('foo'));
+    	$this->assertCount(1, $this->api->getActions());    	
+    }
+
+    public function testDropTable()
+    {    	
+    	$this->assertNull($this->api->dropTable('foo'));
+    	$this->assertCount(1, $this->api->getActions());
+    }
+
+    public function testGetActions()
+    {
+    	$this->api->dropTable('foo');
+    	$this->assertCount(1, $this->api->getActions());
+
+    	$this->api->dropTable('bar');
+    	$this->assertCount(2, $this->api->getActions());
+    }
+}
+
+class FooApi extends MigrationApi
+{
+
+}
