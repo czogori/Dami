ef4f90aa180881efe34f08ea167731cb588dfc2c
Arek Jaskólski <arek.jaskolski@gmail.com>
2013-09-29 23:30:40 +0200

    Added tests

---
+++ tests/Migration/FileNameParserTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Dami\Tests\Migration;
+
+use Dami\Migration\FileNameParser;
+
+/**
+ * @author Arek Jaskólski <arek.jaskolski@gmail.com>
+ */
+class FileNameParserTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $fileName = '20130624094755_add_table_foo.php';
+        $this->fileNameParser = new FileNameParser($fileName);                
+    }
+
+    public function test_getVersion()
+    {
+        $this->assertEquals('20130624094755', $this->fileNameParser->getVersion());
+    }
+
+    public function test_getMigratrionName()
+    {
+        $this->assertEquals('add_table_foo', $this->fileNameParser->getMigrationName());
+    }
+
+    public function test_getMigrationClassName()
+    {            
+        $this->assertEquals('AddTableFooMigration', $this->fileNameParser->getMigrationClassName());
+    }
+}
