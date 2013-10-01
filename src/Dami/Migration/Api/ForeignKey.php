4a7107be51ce78e7611c1f593cd0123eac38d398
Arek Jaskólski <arek.jaskolski@gmail.com>
2013-10-01 23:14:45 +0200

    Support for schema

--- src/Dami/Migration/Api/ForeignKey.php
+++ src/Dami/Migration/Api/ForeignKey.php
@@ -4,20 +4,38 @@ namespace Dami\Migration\Api;
 
 use Rentgen\Database\Constraint\ForeignKey as RentgenForeignKey;
 use Rentgen\Database\Table as RentgenTable;
+use Rentgen\Schema\Info;
 
 class ForeignKey extends RentgenForeignKey
 {
-    public function foreignKey($tableName, $columnNames)
+    private $info;
+
+    public function __construct(Info $info)
     {
-        $this->setTable(new RentgenTable($tableName));
+        $this->info = $info;
+    }
+
+    public function foreignKey($tableName, $columnNames, array $options = array())
+    {
+        $table = new RentgenTable($tableName);
+
+        if (isset($options['schema'])) {
+            $table->setSchema($options['schema']);
+        }
+        $this->setTable($table);
         $this->setColumns($columnNames);
 
         return $this;
     }
 
-    public function reference($tableName, $columnNames)
+    public function reference($tableName, $columnNames, array $options = array())
     {
-        $this->setReferencedTable(new RentgenTable($tableName));
+        $table = new RentgenTable($tableName);
+
+        if (isset($options['schema'])) {
+            $table->setSchema($options['schema']);
+        }
+        $this->setReferencedTable($table);
         $this->setReferencedColumns($columnNames);
 
         return $this;
