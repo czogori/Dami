00dc027bfac709f752e20c83fe9a07df73312e18
Arek Jaskólski <arek.jaskolski@gmail.com>
2013-11-15 01:04:21 +0100

    Added checking valid actions and objects

--- src/Dami/Migration/MigrationNameParser.php
+++ src/Dami/Migration/MigrationNameParser.php
@@ -10,32 +10,57 @@ class MigrationNameParser
     private $action;
     private $actionObject;
     private $model;
+    private $validActions = array('create', 'add', 'drop', 'remove');
+    private $validActionObjects = array('table', 'column', 'index');
 
-    private $availableActions = array('Create', 'Add', 'Drop', 'Remove');
-    private $availableActionObjects = array('Table', 'Column', 'Index');
-
+    /**
+     * Sets a migration name.
+     *
+     * @param string $migrationName A migration name.
+     *
+     * @return void
+     */
     public function setMigrationName($migrationName)
     {
         $this->migrationName = $migrationName;
         $migrationNameAsUnderscoreSrtring = StringHelper::underscore($migrationName);
         $items = explode('_', $migrationNameAsUnderscoreSrtring);
         if (count($items) >= 3) {
-            $this->action = $items[0];
-            $this->actionObject = $items[1];
+            $this->action = in_array($items[0], $this->validActions)
+                ? $items[0]
+                : null;            
+            $this->actionObject = in_array($items[1], $this->validActionObjects)
+                ? $items[1]
+                : null;
             $this->model = str_replace($items[0] . '_' . $items[1] . '_', '', $migrationNameAsUnderscoreSrtring);
         }
     }
 
+    /**
+     * Gets an action name of migration.
+     *
+     * @return string Action name.
+     */
     public function getAction()
     {
         return $this->action;
     }
 
+    /**
+     * Gets an object  of action.
+     *
+     * @return string Object action name.
+     */
     public function getActionObject()
     {
         return $this->actionObject;
     }
 
+    /**
+     * Gets a model name of migration.
+     *
+     * @return string Model name.
+     */
     public function getModel()
     {
         return $this->model;
