de483e9a58c2b3a633b91c005bbccc6c005d06ef
Arek Jaskólski <ajaskolski@gromar.eu>
2019-02-12 15:52:02 +0100

    Refactor

--- src/Dami/EventListener/SqlSubscriber.php
+++ src/Dami/EventListener/SqlSubscriber.php
@@ -19,7 +19,7 @@ class SqlSubscriber implements EventSubscriberInterface
     {
         $sql = $event->getSql();
         if ($this->verbosity->isVerbose() && !in_array(strtolower($sql), ['begin', 'commit', 'rollback'])) {
-            echo sprintf("\n%s\n", $event->getSql());
+            echo sprintf("\n%s\n", $sql);
         }
     }
 
